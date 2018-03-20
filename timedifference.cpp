52235571f7263f4afc57a637088d415c7bca1517
yuvaraj9225 <35728139+yuvaraj9225@users.noreply.github.com>
2018-03-20 03:49:15 -0700

    Create timedifference.cpp

---
+++ timedifference.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d,e,f;
+cout<<"hours: ";
+cin>>a;
+cout<<" minutes \t";
+cin>>b;
+cout<<"\n hours:";
+cin>>c;
+cout<<" minutes:\t";
+cin>>d;
+e=a-c;
+f=b-d;
+cout<<"\nhours:\t"<<e;
+cout<<" minutes\t"<<f;
+}
